cb13b7ba2259ddf1576473e1bdc806f50456d335
Irving Valencia <75141924+irving-vale@users.noreply.github.com>
2024-07-29 12:29:36 -0600

    fix: se compuso el problema que no aparecia el menu, solo se agrego la animacion de primeNg

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -4,21 +4,15 @@ import { MenuItem } from 'primeng/api';
 @Component({
   selector: 'shared-menu',
   templateUrl: './menu.component.html',
-  
 })
 export class MenuComponent implements OnInit {
-
   public menuItems: MenuItem[] = [];
-
 
   ngOnInit() {
     this.menuItems = [
-        { label: 'New', icon: 'pi pi-plus' },
-        { label: 'Open', icon: 'pi pi-dowload'},
-        { label: 'Undo', icon: 'pi pi-refresh' }
+      { label: 'New', icon: 'pi pi-plus' },
+      { label: 'Open', icon: 'pi pi-download' },
+      { label: 'Undo', icon: 'pi pi-refresh' },
     ];
-}
-
-
-
+  }
 }
