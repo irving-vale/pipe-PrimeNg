61c7ac4e7f4b8e06514a71903c8ba42d4e71ea73
Irving Valencia <75141924+irving-vale@users.noreply.github.com>
2024-07-29 12:16:41 -0600

    primer commit creacion del proyecto

---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  
+})
+export class MenuComponent implements OnInit {
+
+  public menuItems: MenuItem[] = [];
+
+
+  ngOnInit() {
+    this.menuItems = [
+        { label: 'New', icon: 'pi pi-plus' },
+        { label: 'Open', icon: 'pi pi-dowload'},
+        { label: 'Undo', icon: 'pi pi-refresh' }
+    ];
+}
+
+
+
+}
